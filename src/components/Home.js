4a0fcfb8e81151bd68599d5de08f9271f532ff75
joaquindiazs <joaquindiazschillagi@gmail.com>
2020-01-29 12:02:24 +0100

    Add slider images. Add package "swiper": "^5.3.0"

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import Swiper from 'react-id-swiper';
+import 'swiper/css/swiper.css'
+import SlideOne from '../assets/img/slide-01.jpg';
+import SlideTwo from '../assets/img/slide-02.jpg';
+import SlideThree from '../assets/img/slide-03.jpg';
+
 import './Home.css'
 
 class Home extends Component {
 	render() {
+		const params = {
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'bullets',
+				clickable: true
+			},
+		}
 		return(
 			<div className="home-wrapper">
-				HOME
+				<Swiper {...params}>
+				    <div style={{'backgroundImage': `url(${SlideOne})`}}>SLIDE 1</div>
+				    <div style={{'backgroundImage': `url(${SlideTwo})`}}>SLIDE 2</div>
+				    <div style={{'backgroundImage': `url(${SlideThree})`}}>SLIDE 3</div>
+				</Swiper>
+				<div className="otraCosa">
+					asdfasdf 
+					asdfasdf
+					asdfasdf
+				</div>
 			</div>
 		)
 	}
